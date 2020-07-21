a25be88b4183c1d03f77f67d9a047eb94696808c
Anthony Sokry <anthonysokry11@gmail.com>
2020-07-20 22:56:48 -0700

    my test

--- sketch.js
+++ sketch.js
@@ -1,5 +1,15 @@
 var cnv;
 let music;
+var fft;
+
+let dots = [];
+var total = 30;
+
+var numOfBands = 256;
+var w;
+
+// var wPerCol;
+// var numOfCol = 20;
 
 function centerCanvas() {
   var x = (windowWidth - width) / 2;
@@ -8,19 +18,32 @@ function centerCanvas() {
 }
 
 function preload() {
-  music = loadSound("music/hello_how_are_you.mp3", loaded);
+  music = loadSound("https://asokry.github.io/myP5Summer/music/hello_how_are_you.mp3", loaded);
 }
 
 function setup() {
   cnv = createCanvas(windowWidth, windowHeight);
   centerCanvas();
   cnv.parent('sketch-holder');
+  music.setVolume(0.1);
+  fft = new p5.FFT(0.9,numOfBands);
+
+  background(207,255,229);
+  for (var i = 0; i < total; i++) {
+    dots[i] = new Dot();
+  }
+
+  w = width / numOfBands;
+
+  // wPerCol = abs(width / numOfCol);
+  // for(var i = 0; i < numOfCol; i++){
+  //   rect(i * wPerCol,0, wPerCol,height);
+  // }
 }
 
 function windowResized(){
   //resizeCanvas(windowWidth, windowHeight);
   centerCanvas();
-  background(207,255,229);
 }
 
 function loaded() {
@@ -30,16 +53,55 @@ function loaded() {
 
 function draw() {
   background(207,255,229);
+  var spectrum = fft.analyze();
+  for(var sp=0; sp<spectrum.length; sp++){
+    var amp = spectrum[sp];
+    var ysp = map(amp, 0, 256, height,0);
+    stroke(0);
+    line(sp*w, height, sp*w, ysp);
+  }
+
+  for(var i=0; i<dots.length; i++){
+    dots[i].update();
+    dots[i].draw();
+  }
+
+  //rect(0, height-100, width, 100);
 }
 
 function mousePressed() {
   if (music.isPlaying()) {
     // .isPlaying() returns a boolean
     music.pause(); // .play() will resume from .pause() position
-    background(255, 0, 0);
+    console.log("Paused");
   } else {
     music.play();
     console.log('Playing');
-    background(0, 255, 0);
   }
+}
+
+function Dot() {
+  this.size = 40;
+  this.speed = 5;
+
+  this.init = function(){
+    this.x = random(this.size, width - this.size);
+    //this.x = startX;
+    this.y = random(-height, -this.size);
+  }
+
+  this.draw = function() {
+    noStroke();
+    fill(255);
+    circle(this.x,this.y, this.size);
+  }
+
+  this.update = function() {
+    this.y += this.speed;
+    if(this.y - this.size > height) {
+      this.init();
+    }
+  }
+
+  this.init();
 }
